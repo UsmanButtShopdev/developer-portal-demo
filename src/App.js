8ddffaf8e3c093045ba7c4770ca69c3233e20268
Muhammad Usman Butt <usman.butt1990@gmail.com>
2024-06-28 15:42:00 +0500

    feat: added fonts and updated style and look

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { ApiReferenceReact } from '@scalar/api-reference-react'
+import { ApiReferenceReact } from '@scalar/api-reference-react' // https://github.com/scalar/scalar/blob/main/documentation/configuration.md
 import { GoogleLogin } from '@react-oauth/google';
 import { Breadcrumb, Layout, Menu, theme, Segmented } from 'antd';
 import documentations from "./sample-docs";
-import '@scalar/api-reference-react/style.css'
+import '@scalar/api-reference-react/style.css' //https://github.com/scalar/scalar/blob/main/documentation/themes.md
 import './App.css';
 
 const { Header, Content, Footer } = Layout;
@@ -23,9 +23,10 @@ const items = [{
 
 function App() {
   useEffect(() => {
-    console.log(documentations);
+    localStorage.setItem("isDark", "false");
   }, [])
   const [ selectedSegment, setSelectedSegment ] = useState('Preview');
+  const [scalarTheme, setScalarTheme ] = useState('kepler');
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
@@ -64,10 +65,19 @@ function App() {
         onChange={(value) => setSelectedSegment(value)}
         options={['JSON', 'Preview', 'V1', 'withAuth', 'HP Long API']}
       />
+       {/* <Segmented
+        defaultValue={scalarTheme}
+        style={{ margin: "12px 0" }}
+        onChange={(value) => setScalarTheme(value)}
+        options={['alternate', 'default', 'moon', 'purple', 'solarized',
+'bluePlanet', 'saturn', 'kepler', 'mars', 'deepSpace', 'none']}
+      /> */}
         </div>
         <ApiReferenceReact
           configuration={{
             spec: documentations[selectedSegment],
+            withDefaultFonts: 'false',
+            theme: scalarTheme,
           }}
         />
       </Content>
